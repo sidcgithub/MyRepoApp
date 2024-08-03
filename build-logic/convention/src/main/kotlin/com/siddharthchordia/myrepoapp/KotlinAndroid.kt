ba7560da2e53d9e5de037a588aa329c869cb0b78
schordia <siddharth.chordia@gmail.com>
2024-08-03 11:31:28 -0400

    MyRepoApp-2: Introduced Application and Compose convention plugins

---
+++ build-logic/convention/src/main/kotlin/com/siddharthchordia/myrepoapp/KotlinAndroid.kt
@@ -0,0 +1,78 @@
+package com.siddharthchordia.myrepoapp
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.provideDelegate
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinTopLevelExtension
+
+/**
+ * Configures the Android project with Kotlin.
+ *
+ * @param commonExtension The common extension for Android projects.
+ */
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 34
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+            isCoreLibraryDesugaringEnabled = true
+        }
+    }
+
+    // Configures the Kotlin extension for the Android project.
+    configureKotlin<KotlinAndroidProjectExtension>()
+
+    dependencies {
+        // Adds the core library desugaring dependency.
+        add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+    }
+}
+
+/**
+ * Configures the JVM project with Kotlin.
+ */
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    // Configures the Kotlin extension for the JVM project.
+    configureKotlin<KotlinJvmProjectExtension>()
+}
+
+/**
+ * Configures the Kotlin extension for the project.
+ *
+ * @param T The type of the Kotlin extension.
+ */
+private inline fun <reified T : KotlinTopLevelExtension> Project.configureKotlin() = configure<T> {
+    val warningsAsErrors: String? by project
+    when (this) {
+        is KotlinAndroidProjectExtension -> compilerOptions
+        is KotlinJvmProjectExtension -> compilerOptions
+        else -> TODO("Implement for other Kotlin extensions.")
+    }.apply {
+        jvmTarget = JvmTarget.JVM_11
+        allWarningsAsErrors = warningsAsErrors.toBoolean()
+        freeCompilerArgs.add(
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        )
+    }
+}
