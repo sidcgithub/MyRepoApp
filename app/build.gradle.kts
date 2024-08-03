088dac4178493ee2ff0c55f6d7126c26a87cf92d
schordia <siddharth.chordia@gmail.com>
2024-08-03 11:58:19 -0400

    MyRepoApp-2: Removed redundant dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
